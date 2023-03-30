2a534efe6fa83bebf33f72a23df7f7ac868f0c8e
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-03-30 11:04:17 +0530

    Sum Of Two Number Question

---
+++ JavaBasicQuestion/src/Basic/InputSumNum.java
@@ -0,0 +1,18 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class InputSumNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number : ");
+        int b = sc.nextInt();
+        System.out.println("Sum of Two Number : " + sumOfTwoNum(a,b));
+    }
+    public static int sumOfTwoNum(int a,int b){
+        return (a+b);
+    }
+}
+
