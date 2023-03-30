33ee26a89c7352531bb91ba6d5f0b91fc528cce7
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-03-30 10:58:56 +0530

    Sum Of Two Number Question

---
+++ JavaBasicQuestion/src/Basic/SumOfTwoNum.java
@@ -0,0 +1,13 @@
+package Basic;
+
+public class SumOfTwoNum {
+    public static void main(String[] args) {
+        System.out.println(SumOfTwoNum(10,20));
+        System.out.println("Sum of Two Number : " + (SumOfTwoNum(20,40)));
+    }
+
+    public static int SumOfTwoNum(int a,int b){
+        return (a+b);
+    }
+
+}
