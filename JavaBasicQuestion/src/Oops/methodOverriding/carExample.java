3fb3b3260925af18c9b51103c025cb07d3c4b307
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-03 10:23:12 +0530

    car example added

---
+++ JavaBasicQuestion/src/Oops/methodOverriding/carExample.java
@@ -0,0 +1,34 @@
+package Oops.methodOverriding;
+class car{
+    public void start(){
+        System.out.println("Car has started");
+    }
+    public void changeGear(){
+        System.out.println("Car has changed gear");
+    }
+}
+class luxCar extends car{
+    @Override
+    public void start(){
+        System.out.println("LuxCar has started");
+    }
+    @Override
+    public void changeGear(){
+        System.out.println("LuxCar has changed gear");
+    }
+    public void musicSys(){
+        System.out.println("LuxCar has music System");
+    }
+}
+public class carExample {
+    public static void main(String[] args) {
+        car c = new car();
+        c.start();
+        c.changeGear();
+        luxCar lc = new luxCar();
+        lc.start();
+        lc.changeGear();
+        lc.musicSys();
+
+    }
+}
