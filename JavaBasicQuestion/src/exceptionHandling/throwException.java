089c3055d25c193d2fd237a0eb250cb82ab22105
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-05 13:38:21 +0530

    Throw and Throws example added

---
+++ JavaBasicQuestion/src/exceptionHandling/throwException.java
@@ -0,0 +1,21 @@
+package exceptionHandling;
+
+public class throwException {
+    static void meth1(){
+        try {
+            int a=meth2(-10,5);
+            System.out.println(a);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    static int meth2(int l,int b) throws Exception{
+        if(l<0 || b <0 )
+            throw new Exception("Length and breadth cannot be negative");
+        return l*b;
+    }
+
+    public static void main(String[] args) {
+        meth1();
+    }
+}
