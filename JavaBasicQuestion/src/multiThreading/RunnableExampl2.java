fb358c320e0e9714629d3f0ae5496a9dae9ab897
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-07 07:11:11 +0530

    Runnable Example 2 added

---
+++ JavaBasicQuestion/src/multiThreading/RunnableExampl2.java
@@ -0,0 +1,22 @@
+package multiThreading;
+
+public class RunnableExampl2  implements Runnable{
+    @Override
+    public void run() {
+        int i = 1;
+        while (true){
+            System.out.println(i + " Hello");
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        RunnableExampl2 r2 = new RunnableExampl2();
+        Thread th = new Thread(r2);
+        th.start();
+        int i = 1;
+        while (true){
+            System.out.println(i + " World");
+        }
+    }
+}
