65d0a3d87d3a571da56a6e9311aae2616034f90b
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-12 23:26:07 +0530

    Runnable Example 3 modified

--- JavaBasicQuestion/src/multiThreading/example3.java
+++ JavaBasicQuestion/src/multiThreading/example3.java
@@ -13,7 +13,7 @@ public class RunnableExampl2  implements Runnable{
     public static void main(String[] args) {
         RunnableExampl2 r2 = new RunnableExampl2();
         Thread th = new Thread(r2);
-        th.start();
+//        th.start();
 //        th.start();
         int i = 1;
         while (true){
