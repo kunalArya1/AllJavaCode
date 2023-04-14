ce96ac24d7b40d66c6ac82d0db108f47ca40b089
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-14 22:15:32 +0530

    Runnable Example 3 modified

--- JavaBasicQuestion/src/multiThreading/example3.java
+++ JavaBasicQuestion/src/multiThreading/example3.java
@@ -28,7 +28,6 @@ public class RunnableExampl2  implements Runnable{
             System.out.println(i + "World ");
             System.out.println(i + "World ");
             System.out.println(i + "World ");
-            System.out.println(i + "World ");
 
 
 
