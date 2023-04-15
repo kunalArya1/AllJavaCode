535e14d1780d9aa6e8e7c4c47b507c53852019e8
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-15 13:13:34 +0530

    Simple Interest question (Q.N - 9 ) added

---
+++ JavaBasicQuestion/src/Basic/nine.java
@@ -0,0 +1,19 @@
+package Basic;
+
+import java.util.Scanner;
+
+//Accept the Principle amount, time & rate of interest and calculate the Simple Interest
+public class nine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Amount :- ");
+        double amount = sc.nextDouble();
+        System.out.print("Enter time :- ");
+        double time = sc.nextDouble();
+        System.out.print("Enter rate :- ");
+        double rate = sc.nextDouble();
+        double si = ((amount*time*rate)/100);
+        System.out.println("Simple interest is :-  " + si);
+
+    }
+}
