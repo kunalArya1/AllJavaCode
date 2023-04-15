d7aa73a9603f4d401b7ef7c6179bed68bc785fa4
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-15 13:37:36 +0530

    Character input question (Q.N - 12 ) added

---
+++ JavaBasicQuestion/src/Basic/Twelve.java
@@ -0,0 +1,23 @@
+package Basic;
+
+
+import java.util.Scanner;
+
+//12- Accept the gender from the user as char and print the respective greeting message
+//     Ex - Good Morning Sir (on the basis of gender)
+public class Twelve {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your gender in (M/F) :- ");
+        String gender = sc.nextLine();
+        char genderChar = gender.charAt(0);
+        if (genderChar == 'M'){
+            System.out.println("Good Morning Sir.....");
+        }else if (genderChar == 'F'){
+            System.out.println("Good Morning Ma'am....");
+        }
+        else {
+            System.out.println("Oh! Wrong Input....");
+        }
+    }
+}
