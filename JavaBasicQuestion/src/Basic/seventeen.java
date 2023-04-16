03edb1297bc65ac122654a47a4719195870f6e6a
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-16 12:32:43 +0530

    Herons Formula question (Q.N - 17 ) added

--- JavaBasicQuestion/src/Basic/seventeen.java
+++ JavaBasicQuestion/src/Basic/seventeen.java
@@ -1,7 +1,20 @@
 package Basic;
+
+import java.util.Scanner;
+
 //Accept the three sides of triangle and calculate the area using herons formula
 public class seventeen {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first side of triangle :- ");
+        int fSide = sc.nextInt();
+        System.out.print("Enter Second side of triangle :- ");
+        int sSide = sc.nextInt();
+        System.out.print("Enter third side of triangle :- ");
+        int tSide = sc.nextInt();
+        int semiP = ((fSide+sSide+tSide)/2);
 
+        double area = Math.sqrt((semiP*(semiP-fSide)*(semiP-sSide)*(semiP-tSide)));
+        System.out.println("Area is :- " + area);
     }
 }
