6def7052a81cf35a31e800406afdbdeeb7a0c7ee
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-17 21:19:43 +0530

    Sum of all factors question (Q.N - 37 ) added

---
+++ JavaBasicQuestion/src/Basic/Q_37_SumOfAllFactor.java
@@ -0,0 +1,9 @@
+package Basic;
+
+//Print the sum of all factors of a number, 50 - 1 + 2 + 5 + 10 + 25 = 43
+public class Q_37_SumOfAllFactor {
+    public static void main(String[] args) {
+
+    }
+
+}
