036747773ed4db53def600726fbd1e5fef201e57
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-17 21:21:05 +0530

    print digit question (Q.N - 40 ) added

---
+++ JavaBasicQuestion/src/Basic/Q_40_PrintDigit.java
@@ -0,0 +1,16 @@
+package Basic;
+
+
+import java.util.Scanner;
+
+//Seprate each digit of a number and print it on the new line
+public class Q_40_PrintDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number :- ");
+        int n = sc.nextInt();
+//        for (int i = 1; i < ; i++) {
+//
+//        }
+    }
+}
