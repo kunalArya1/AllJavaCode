56db3c13f6849e75c69b60b3d4b417c0be09c2f6
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-17 21:23:02 +0530

    Pallindromic Number question (Q.N - 44 ) added

---
+++ JavaBasicQuestion/src/Basic/Q_44_PallindromicNumber.java
@@ -0,0 +1,10 @@
+package Basic;
+
+
+//Accept a number and check if it is a pallindromic number (If number and its reverse are equal)
+//       Ex - 12321 - Rerverse - 12321
+public class Q_44_PallindromicNumber {
+    public static void main(String[] args) {
+
+    }
+}
