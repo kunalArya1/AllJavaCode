da7bd94a078221fb285299764a68b3b782927007
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-17 21:20:10 +0530

    all factors question (Q.N - 38 ) added

---
+++ JavaBasicQuestion/src/Basic/Q_38_AllFactors.java
@@ -0,0 +1,12 @@
+package Basic;
+
+import java.util.Scanner;
+
+//Print all the factors of a number.
+public class Q_38_AllFactors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+    }
+}
