20c32ff49b7e38d98fbbbf1fdb1c254623d839ab
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-18 23:56:11 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_42_Prime.java
+++ JavaBasicQuestion/src/Basic/Q_42_Prime.java
@@ -1,8 +1,10 @@
 package Basic;
 
+import java.util.Scanner;
+
 //Check if the number is Prime or not.
 public class Q_42_Prime {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
