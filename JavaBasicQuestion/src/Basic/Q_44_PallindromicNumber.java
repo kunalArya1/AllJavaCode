2d187f35c9f3db03dcde388eac42a941b8dcd7a7
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-18 23:56:46 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_44_PallindromicNumber.java
+++ JavaBasicQuestion/src/Basic/Q_44_PallindromicNumber.java
@@ -1,10 +1,12 @@
 package Basic;
 
 
+import java.util.Scanner;
+
 //Accept a number and check if it is a pallindromic number (If number and its reverse are equal)
 //       Ex - 12321 - Rerverse - 12321
 public class Q_44_PallindromicNumber {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
