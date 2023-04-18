9d957771a2c74da4c547774d2ff214430ccb4171
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-18 23:53:20 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_34_Factorial.java
+++ JavaBasicQuestion/src/Basic/Q_34_Factorial.java
@@ -1,9 +1,11 @@
 package Basic;
 
 
+import java.util.Scanner;
+
 //Factorial of a number
 public class Q_34_Factorial {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
