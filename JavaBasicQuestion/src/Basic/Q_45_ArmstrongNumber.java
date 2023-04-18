9350bc646a02d3429680e26a51317679ce5fdfb0
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-18 23:57:13 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_45_ArmstrongNumber.java
+++ JavaBasicQuestion/src/Basic/Q_45_ArmstrongNumber.java
@@ -1,11 +1,13 @@
 package Basic;
 
 
+import java.util.Scanner;
+
 //Accept a number and check if it is a armstrong number (Sum of cube of all digits will be equal to origional number)
 //       Ex - 407 = 64 + 0 + 343 = 407
 //              153 = 1 + 125 + 27 = 153
 public class Q_45_ArmstrongNumber {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
