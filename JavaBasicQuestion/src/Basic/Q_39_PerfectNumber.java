5fbc0284aaaa1f663b91cc8e23877945b1987615
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-18 23:55:15 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_39_PerfectNumber.java
+++ JavaBasicQuestion/src/Basic/Q_39_PerfectNumber.java
@@ -1,8 +1,11 @@
 package Basic;
+
+import java.util.Scanner;
+
 //Accept a number and check if it a perfect number or not.A number whose sum of factors(excluding number itself)  = Number
 //Ex -  6 = 1, 2, 3 = 6
 public class Q_39_PerfectNumber {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
