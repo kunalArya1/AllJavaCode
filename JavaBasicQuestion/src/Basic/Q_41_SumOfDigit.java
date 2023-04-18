0ed02a83691782aba13556bfc169191c7b70e19c
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-18 23:55:55 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_41_SumOfDigit.java
+++ JavaBasicQuestion/src/Basic/Q_41_SumOfDigit.java
@@ -1,8 +1,10 @@
 package Basic;
 
+import java.util.Scanner;
+
 //Sum of digits of a number, 936 = 18
 public class Q_41_SumOfDigit {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
