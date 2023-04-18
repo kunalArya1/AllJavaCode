6a5d302cf35edc498c54cf6a5fa7875947faf38c
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-18 23:56:31 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_43_RevrseANumber.java
+++ JavaBasicQuestion/src/Basic/Q_43_RevrseANumber.java
@@ -1,9 +1,11 @@
 package Basic;
 
 
+import java.util.Scanner;
+
 //Accept a number and print its reverse
 public class Q_43_RevrseANumber {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
