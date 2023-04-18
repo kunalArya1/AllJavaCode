d55faea91558a600a27b2be1f72114d6f3b040a9
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-18 23:54:33 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_37_SumOfAllFactor.java
+++ JavaBasicQuestion/src/Basic/Q_37_SumOfAllFactor.java
@@ -1,9 +1,11 @@
 package Basic;
 
+import java.util.Scanner;
+
 //Print the sum of all factors of a number, 50 - 1 + 2 + 5 + 10 + 25 = 43
 public class Q_37_SumOfAllFactor {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 
 }
