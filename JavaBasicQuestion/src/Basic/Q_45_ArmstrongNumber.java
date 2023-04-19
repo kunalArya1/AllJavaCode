875b55eefccd274bb3a318bdc6651a366df15e3d
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-19 23:23:45 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_45_ArmstrongNumber.java
+++ JavaBasicQuestion/src/Basic/Q_45_ArmstrongNumber.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 public class Q_45_ArmstrongNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+
     }
 }
