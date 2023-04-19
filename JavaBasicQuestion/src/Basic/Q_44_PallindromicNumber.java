28f1e7a7dd051555feb6c5bb7bf03c891794f18b
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-19 23:23:22 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_44_PallindromicNumber.java
+++ JavaBasicQuestion/src/Basic/Q_44_PallindromicNumber.java
@@ -8,5 +8,6 @@ import java.util.Scanner;
 public class Q_44_PallindromicNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
     }
 }
