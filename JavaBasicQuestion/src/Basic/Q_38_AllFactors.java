230da3d70a75ed84426bf586a9cc88581ece50fe
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-20 21:41:05 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_38_AllFactors.java
+++ JavaBasicQuestion/src/Basic/Q_38_AllFactors.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Q_38_AllFactors {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+//        int num = sc.nextInt();
 
     }
 }
