ccc4b96ee19ffd6f927fbd62c1b285a8abe84a41
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-20 21:42:09 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_42_Prime.java
+++ JavaBasicQuestion/src/Basic/Q_42_Prime.java
@@ -6,6 +6,6 @@ import java.util.Scanner;
 public class Q_42_Prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+//        int a = sc.nextInt();
     }
 }
