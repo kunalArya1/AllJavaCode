d0f594ea35a808fefacbc4fb5cca9fec9ec613ad
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-22 23:28:52 +0530

    .

--- JavaBasicQuestion/src/Basic/Q_40_PrintDigit.java
+++ JavaBasicQuestion/src/Basic/Q_40_PrintDigit.java
@@ -8,7 +8,7 @@ public class Q_40_PrintDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number :- ");
-//        int n = sc.nextInt();
+        int n = sc.nextInt();
 //        for (int i = 1; i < ; i++) {
 //
 //        }
