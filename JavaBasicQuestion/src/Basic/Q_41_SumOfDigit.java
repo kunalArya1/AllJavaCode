176093a079807e483ba69eb49f174e6f236e7240
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-23 13:25:15 +0530

    Sum of digit question (Q.N - 40 ) added

--- JavaBasicQuestion/src/Basic/Q_41_SumOfDigit.java
+++ JavaBasicQuestion/src/Basic/Q_41_SumOfDigit.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 public class Q_41_SumOfDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        System.out.print("Enter a Number :- ");
+        int num = sc.nextInt();
+        int digitSum = 0;
+        while (num >0){
+            int digit = num%10;
+            digitSum += digit;
+            num = num /10;
+
+        }
+        System.out.println("Sum of Digit :- " + digitSum);
+
     }
 }
