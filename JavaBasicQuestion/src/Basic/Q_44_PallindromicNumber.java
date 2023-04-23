b0011fa8df6f8519d730bd8671d72d21d93e4a1e
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-23 14:12:17 +0530

    Pallindrom Number question (Q.N - 41 ) added

--- JavaBasicQuestion/src/Basic/Q_44_PallindromicNumber.java
+++ JavaBasicQuestion/src/Basic/Q_44_PallindromicNumber.java
@@ -8,6 +8,22 @@ import java.util.Scanner;
 public class Q_44_PallindromicNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        int a = sc.nextInt();
+        System.out.println("Enter your Number :- ");
+        int num = sc.nextInt();
+        pallNumber(num);
+    }
+    public static void pallNumber(int num ){
+        int r,sum=0,temp;
+
+        temp=num;
+        while(num>0){
+            r=num%10;
+            sum=(sum*10)+r;
+            num=num/10;
+        }
+        if(temp==sum)
+            System.out.println("palindrome number ");
+        else
+            System.out.println("not palindrome");
     }
 }
