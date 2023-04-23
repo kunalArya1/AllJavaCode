23039ba143b377cdd5a6fc04c88dc3c042a270d2
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-23 14:19:26 +0530

    Pallindrom Number question (Q.N - 41 ) added

--- JavaBasicQuestion/src/Basic/Q_44_PallindromicNumber.java
+++ JavaBasicQuestion/src/Basic/Q_44_PallindromicNumber.java
@@ -4,7 +4,7 @@ package Basic;
 import java.util.Scanner;
 
 //Accept a number and check if it is a pallindromic number (If number and its reverse are equal)
-//       Ex - 12321 - Rerverse - 12321
+// Ex - 12321 - Rerverse - 12321
 public class Q_44_PallindromicNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,7 +14,6 @@ public class Q_44_PallindromicNumber {
     }
     public static void pallNumber(int num ){
         int r,sum=0,temp;
-
         temp=num;
         while(num>0){
             r=num%10;
