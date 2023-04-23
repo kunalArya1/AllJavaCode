3caa6be330a2228e8a4852877a4fde04ab5a0163
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-23 13:39:14 +0530

    Reverse a Number question (Q.N - 41 ) added

--- JavaBasicQuestion/src/Basic/Q_43_RevrseANumber.java
+++ JavaBasicQuestion/src/Basic/Q_43_RevrseANumber.java
@@ -7,6 +7,17 @@ import java.util.Scanner;
 public class Q_43_RevrseANumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        System.out.print("Enter a Number :- ");
+        int num = sc.nextInt();
+        rNumWhile(num);
+    }
+    public static void rNumWhile(int num){
+        int reverse = 0;
+        while (num >0){
+            int rem = num %10;
+            reverse = reverse *10  + rem;
+            num = num /10;
+        }
+        System.out.println("Revrese of Number :- " + reverse);
     }
 }
