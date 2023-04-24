64465adfca2bb0f0f7817ab8557e6782ca954ebc
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-24 19:31:04 +0530

    Armstrong Number  question (Q.N - 45 ) added

--- JavaBasicQuestion/src/Basic/Q_45_ArmstrongNumber.java
+++ JavaBasicQuestion/src/Basic/Q_45_ArmstrongNumber.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class Q_45_ArmstrongNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
 
     }
 }
