8c9b834446b88518e7e186ee42ff2bbc1099e4ab
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-24 19:31:44 +0530

    Strong Number  question (Q.N - 46 ) added

---
+++ JavaBasicQuestion/src/Basic/Q_46_StrongNumber.java
@@ -0,0 +1,7 @@
+package Basic;
+
+
+//Accept a number and check if it is a strong number or not (Sum of factorial of each digit)
+//       Ex- 145 = 1! + 4! + 5! = 145
+public class Q_46_StrongNumber {
+}
