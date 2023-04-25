07e2ef533270a2fdfbbc39bf42eb828913124ea0
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-25 17:10:40 +0530

    Automorphic number question (Q.N - 48 ) added

--- JavaBasicQuestion/src/Basic/Q_48_AutomorphicNumber.java
+++ JavaBasicQuestion/src/Basic/Q_48_AutomorphicNumber.java
@@ -1,6 +1,13 @@
 package Basic;
 
 
+import java.util.Scanner;
+
 //Automorphic number 5 = 25 = 625 = 390625
 public class Q_48_AutomorphicNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+    }
 }
