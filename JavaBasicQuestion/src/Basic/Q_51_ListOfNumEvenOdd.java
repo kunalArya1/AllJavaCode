423b8c3b4ab5e4387d80ba3d73a11fe3cdea1771
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-26 22:57:05 +0530

    Strong number Question ( Q.N - 46 ) Added

--- JavaBasicQuestion/src/Basic/Q_51_ListOfNumEvenOdd.java
+++ JavaBasicQuestion/src/Basic/Q_51_ListOfNumEvenOdd.java
@@ -1,5 +1,19 @@
 package JavaBasicQuestion.src.Basic;
 
+
+//Write a program to print the sum of negative numbers, sum of positive even numbers and
+// the sum of positive odd numbers
+//  from a list of numbers (N) entered by the user.
+//  The list terminates when the user enters a zero.
+
+import java.util.Scanner;
+
 public class Q_51_ListOfNumEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number:- ");
+
+        int num = sc.nextInt();
+    }
 
 }
