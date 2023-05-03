8931c1075b79c57617fa0af3fc22b1ebd7eb23a2
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-05-03 10:34:20 +0530

    Question(Q-N 64) added

---
+++ AllJavaCodes/src/Basic/Q_64_DimondOrKite.java
@@ -0,0 +1,37 @@
+package Basic;
+
+import java.util.Scanner;
+
+//       *
+//	    * *
+//	   * * *
+//	  * * * *
+//	 * * * * *
+//	* * * * * *
+//	 * * * * *
+//	  * * * *
+//	   * * *
+//	    * *
+//	     *
+public class Q_64_DimondOrKite {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dimondPattern(n);
+        }
+        public static void dimondPattern(int n ){
+            for (int row = 0; row < 2*n; row++) {
+                int noOfcol = row > n ? 2*n -row : row;
+                int noOfSpace = n - noOfcol;
+                for (int space = 0; space < noOfSpace ; space++) {
+                    System.out.print(" ");
+                }
+                for (int star = 0; star <  noOfcol; star++) {
+                    System.out.print("* ");
+                }
+                System.out.println("");
+
+            }
+        }
+    }
+
