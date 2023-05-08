f1f9793dfe74ef3ccda6491bfe61eb42fb652465
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-05-08 23:02:12 +0530

    .

--- AllJavaCodes/src/Basic/Q_74_GreatestElementInArray.java
+++ AllJavaCodes/src/Basic/Q_74_GreatestElementInArray.java
@@ -1,7 +1,10 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class Q_74_GreatestElementInArray {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }
