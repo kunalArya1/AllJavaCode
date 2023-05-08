4aee47d58d007ca037233d5b45b58d5fb33e568a
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-05-08 23:02:44 +0530

    .

--- AllJavaCodes/src/Basic/Q_76_SecondLargestArray.java
+++ AllJavaCodes/src/Basic/Q_76_SecondLargestArray.java
@@ -1,7 +1,10 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class Q_76_SecondLargestArray {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }
