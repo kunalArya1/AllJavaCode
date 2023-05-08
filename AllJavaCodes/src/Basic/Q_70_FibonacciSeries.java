ca131fd276d31a1f1b76d22d458e384efe2cc4f1
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-05-08 23:00:39 +0530

    .

--- AllJavaCodes/src/Basic/Q_70_FibonacciSeries.java
+++ AllJavaCodes/src/Basic/Q_70_FibonacciSeries.java
@@ -1,7 +1,11 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class Q_70_FibonacciSeries {
     public static void main(String[] args) {
             int a;
+        Scanner sc = new Scanner(System.in);
+
     }
 }
