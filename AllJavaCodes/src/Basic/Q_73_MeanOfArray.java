6662181f2a6ff0670e0e1856c28447784144e900
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-05-08 23:01:32 +0530

    .

--- AllJavaCodes/src/Basic/Q_73_MeanOfArray.java
+++ AllJavaCodes/src/Basic/Q_73_MeanOfArray.java
@@ -1,7 +1,10 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class Q_73_MeanOfArray {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }
