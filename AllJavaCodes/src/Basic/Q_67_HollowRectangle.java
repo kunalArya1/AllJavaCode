3410174b042e53504daf149a3cc6cfea3c71ff33
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-05-08 22:59:51 +0530

    .

--- AllJavaCodes/src/Basic/Q_67_HollowRectangle.java
+++ AllJavaCodes/src/Basic/Q_67_HollowRectangle.java
@@ -1,7 +1,10 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class Q_67_HollowRectangle {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }
