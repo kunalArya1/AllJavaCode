71da6ff31a2748c13696092fc365ffc612a0660a
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-05-15 18:12:48 +0530

    pd

--- AllJavaCodes/src/Basic/Q_72_NSizeArray.java
+++ AllJavaCodes/src/Basic/Q_72_NSizeArray.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Q_72_NSizeArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
         System.out.println("Number");
+        int num = sc.nextInt();
     }
 }
