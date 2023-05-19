68fe9b64dddcb4c282566fff5d5a642498b7bbdc
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-05-19 08:18:28 +0530

    HCF AND LCM

--- AllJavaCodes/src/Basic/Q_69_LCM.java
+++ AllJavaCodes/src/Basic/Q_69_LCM.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Q_69_LCM {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("Enter two Numbers :- ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int maxNum;
+        for (maxNum = Math.max(num1, num2); maxNum <= (num1*num2) ; maxNum++){
+            if (maxNum % num1 == 0 && maxNum % num2 == 0 )
+                break;
+        }
+        System.out.println("LCM of given number is :- " + maxNum);
     }
 }
