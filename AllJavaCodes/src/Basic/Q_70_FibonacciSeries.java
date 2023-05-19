d18429512f1b453ff84669a4ef5c01c4c2d21335
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-05-19 09:04:45 +0530

    FibonacciSeries

--- AllJavaCodes/src/Basic/Q_70_FibonacciSeries.java
+++ AllJavaCodes/src/Basic/Q_70_FibonacciSeries.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 
 public class Q_70_FibonacciSeries {
     public static void main(String[] args) {
-            int a;
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number :- ");
+        int num = sc.nextInt();
+        int n1 = 0 ,n2 = 1, n3 ;
+        System.out.print(n1 +" , " + n2);
+        for (int i = 2; i < num; i++) {
+            n3 = n1 + n2;
+            System.out.print(" , " + n3 );
+            n1=n2;
+            n2=n3;
+        }
+
 
     }
 }
