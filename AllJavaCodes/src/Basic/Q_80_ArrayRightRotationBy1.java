ad119148f87bf5cda9de0f11ad7cf1fc87eda18b
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-06-10 15:54:51 +0530

    82-  Array rotation by 1 element

---
+++ AllJavaCodes/src/Basic/Q_80_ArrayRightRotationBy1.java
@@ -0,0 +1,7 @@
+package Basic;
+
+public class Q_80_ArrayRightRotationBy1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ AllJavaCodes/src/Basic/Q_81_ArrayLeftRotationByKElements.java
@@ -0,0 +1,7 @@
+package Basic;
+
+public class Q_81_ArrayLeftRotationByKElements {
+    public static void main(String[] args) {
+
+    }
+}
