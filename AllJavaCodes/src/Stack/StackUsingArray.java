1abe12bd97b48e299595eeed1f8945f025aa4d75
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-08-02 12:48:25 +0530

    Stack

---
+++ AllJavaCodes/src/Stack/StackUsingArray.java
@@ -0,0 +1,22 @@
+package Stack;
+
+
+class MyStack{
+    int [] arr;
+
+    public MyStack(int val) {
+        arr = new int [val];
+    }
+
+    public static int Size(int [] arr){
+        return arr.length;
+    }
+    public static boolean isEmpty(int [] arr){
+        return arr.length == 0;
+    }
+}
+public class StackUsingArray {
+    public static void main(String[] args) {
+        MyStack s = new MyStack(10);
+    }
+}
